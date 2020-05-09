5437ef74a39f28f8bfc7d825005fca3ca1c3cf95
Adam SOudani <asoudani@email.com>
2020-05-09 15:36:08 -0700

    finished product

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,7 +14,38 @@ var friends = [
         4,
         1
       ]
+  },
+  {
+      "name": "Randy",
+      "scores": [
+        2,
+        4,
+        2,
+        5,
+        1,
+        5,
+        4,
+        3,
+        2,
+        5
+      ]
+  },
+  {
+    "name": "Sandra",
+    "scores": [
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      5
+    ]
   }
+
 ]
   module.exports = friends;
   
